bb2c795be026af125a6b23f2e66b468d85a22612
Bing <web.bioinfo@gmail.com>
2019-12-13 18:03:42 +0700

    office commit for the weekend

--- pdevice/device/urls.py
+++ pdevice/device/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'device'
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('<int:device_id>/detail/', views.detail, name='detail'),
+    path('<int:device_id>/update/', views.update, name='update'),
     path('sample/', views.sample, name='sample'),
-    path('<int:device_id>/detail/', views.detail, name='detail')
+    path('home2/', views.HomePageView.as_view(), name='home2'),
 ]
--- pdevice/device/views.py
+++ pdevice/device/views.py
@@ -2,22 +2,18 @@ from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_list_or_404, render
 from django.urls import reverse
+from django.views.generic.base import TemplateView
 
 from .models import Device
 
 
 # Create your views here.
 def index(request):
-    recent_device = Device.objects.order_by('operated')[:5]
+    recent_device = Device.objects.order_by('operated')[:20]
     context = {
         'recent_device': recent_device
     }
     return render(request, 'index.html', context)
-
-
-def sample(request):
-    return render(request, 'bail.html')
-
 
 def detail(request, device_id):
     # text = f'Requesting detail for device {device_id}'
@@ -29,9 +25,41 @@ def detail(request, device_id):
 
     return render(request, 'detail.html', {'device': device})
 
+def update(request, device_id):
+    try:
+        device = Device.objects.get(pk=device_id)
+    except Device.DoesNotExist:
+        raise Http404("Device does not existed")
+
+    return render(request, 'detail.html', {'device': device})
+
+
+def sample(request):
+    return render(request, 'bail.html')
+
+#Edit a post
+# def edit(request, pk, template_name='Crud/edit.html'):
+#     post= get_object_or_404(Post, pk=pk)
+#     form = PostForm(request.POST or None, instance=post)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('index')
+#     return render(request, template_name, {'form':form})
+
 
 def PostListView(ListView):
     model = Device
     template = 'device/index.html'
     context_of_name = 'devices'
     ordering = ['-updated']
+
+
+
+class HomePageView(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_device'] = Device.objects.all()[:5]
+        return context
